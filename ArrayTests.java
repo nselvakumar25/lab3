45ea6cbea36ddc8564e5a49aa2054f60df3cf649
nselvakumar25 <nselvakumar@ucsd.edu>
2023-10-24 16:46:45 -0700

    update reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,30 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input = { 1, 2, 3 };
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] { 3, 2, 1 }, input);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input = { 1, 2, 3 };
+    int[] output = ArrayExamples.reversed(input);
+    assertArrayEquals(new int[] { 3, 2, 1 }, output);
   }
 }
